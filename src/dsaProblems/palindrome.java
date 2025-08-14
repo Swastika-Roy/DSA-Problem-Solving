a293ed0c79e861631fb3a42315024fac80042574
Swastika Roy <swastikaroytitu@gmail.com>
2025-08-14 09:17:36 +0530

    Problem Solving practice

---
+++ src/dsaProblems/palindrome.java
@@ -0,0 +1,9 @@
+package dsaProblems;
+
+public class palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(str.equals(reversed));
+    }
+}
