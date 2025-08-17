bf801fcd15b4910ef19d12412781243543762286
Swastika Roy <swastikaroytitu@gmail.com>
2025-08-17 19:33:18 +0530

    programming practice

--- src/dsaProblems/fibonacciSeries.java
+++ src/dsaProblems/fibonacciSeries.java
@@ -1,15 +1,13 @@
 package dsaProblems;
 
 public class fibonacciSeries {
+     static int  fib(int n){
+        if(n==0 || n==1) return 1;
+        return fib(n-1)+fib(n-2);
+    }
     public static void main(String[] args) {
-        int n = 10;
-        int n1 = 0;
-        int n2 = 1;
-        for (int i=2;i<n;i++){
-            int n3 = n1+n2;
-            System.out.print(n3+", ");
-            n1 = n2;
-            n2 = n3;
+        for(int i=0;i<=5;i++){
+            System.out.print(fib(i)+" ");
         }
     }
 }
