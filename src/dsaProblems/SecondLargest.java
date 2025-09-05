ec28d01e09faf48707982ded05362c1a31448bc5
Swastika Roy <swastikaroytitu@gmail.com>
2025-09-05 12:01:58 +0530

    Pascal_triangle

--- src/dsaProblems/SecondLargest.java
+++ src/dsaProblems/SecondLargest.java
@@ -1,20 +1,22 @@
 package dsaProblems;
 
 public class SecondLargest {
+   public static int secLargest(int arr[]){
+       int first = Integer.MIN_VALUE;
+       int sec = Integer.MAX_VALUE;
+        for (int num:arr){
+            if(num > first){
+                sec=first;
+                first=num;
+            } else if (num > sec && num!=first) {
+                sec=num;
+            }
+        }
+        return sec;
+   }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        System.out.println(SecondLargest(arr));
-    }
-    public static int SecondLargest(int arr[]){
-        int first = Integer.MIN_VALUE;
-        int sec = Integer.MIN_VALUE;
-        for (int i=0; i < arr.length;i++){
-            if(arr[i] > first){
-               sec = first;
-               first = arr[i];
-            }else if(arr[i] > sec && arr[i] != first)
-                sec = arr[i];
-        }if(sec == Integer.MIN_VALUE) return -1;
-         return sec;
+        System.out.println(secLargest(arr));
     }
 }
