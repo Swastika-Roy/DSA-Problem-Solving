a8fa6c1d598385195818fe7535c6313cad7d52a7
Swastika Roy <swastikaroytitu@gmail.com>
2025-09-10 14:09:38 +0530

    practice coding daily

--- src/dsaProblems/bubbleSort.java
+++ src/dsaProblems/bubbleSort.java
@@ -10,7 +10,7 @@ public class bubbleSort {
     }
     public static void BubbleSort(int[] arr){
         for (int i=0;i< arr.length-1;i++){
-            for (int j=0; j < arr.length-1;j++){
+            for (int j=0; j < arr.length-1-i;j++){
                 if(arr[j+1] < arr[j] ){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
